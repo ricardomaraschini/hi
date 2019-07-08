ff88b72df4cd7a2535c9baecc39ed57617c47a95
Ricardo Maraschini <rmarasch@redhat.com>
2019-07-08 18:12:16 +0200

    increased number on result

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print(time.Now())
-		fmt.Fprint(w, "hi2")
+		fmt.Fprint(w, "hi3")
 	})
 
 	log.Println("listening on :8181")
